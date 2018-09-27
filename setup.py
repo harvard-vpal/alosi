099a520acf89aeb74db4849b2dc50f21d9da71e1
Andrew Ang <andrew_ang@harvard.edu>
2018-09-27 11:46:38 -0400

    v1.2.1 - Engine refactoring and test framework (#13)
    * Use relevance instead of guess and slip in function args
    
    * feat: make BaseAdaptiveEngine more generic
    
    * refactor: merge last attempted slip and guess into relevance param, rename mastery odds params for clarity
    
    * Bump version
    
    * feat: docker runtime and travis config
    
    * Update gitignore

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='alosi',
-    version='1.2.0',
+    version='1.2.1',
     description='Utilities for the ALOSI adaptive learning system',
     url='https://github.com/harvard-vpal/alosi',
     author='Andrew Ang',
