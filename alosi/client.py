5a1ed679f280084122b5f3cc8d3d07cd85089949
Andrew Ang <marc.kunanit@gmail.com>
2019-06-06 13:15:03 -0400

    refactor: make client initialization parameters optional

--- alosi/client.py
+++ alosi/client.py
@@ -4,9 +4,23 @@ from .models import Activity, Collection, KnowledgeComponent
 
 
 class AlosiClient:
-    def __init__(self, *, bridge_host, bridge_token, bridge_owner_pk, engine_host, engine_token, content_source_pk):
-        self.bridge_api = BridgeApi(bridge_host, token=bridge_token)
-        self.engine_api = EngineApi(engine_host, token=engine_token)
+    def __init__(self, *, bridge_host=None, bridge_token=None, bridge_owner_pk=None, engine_host=None, engine_token=None, content_source_pk=None):
+        """
+        Initialize client with configuration and credentials.
+        For some use cases involving subsets of systems, some groups of parameters may not be needed
+        
+        :param bridge_host: Base URL of bridge application
+        :param bridge_token: API token for bridge
+        :param bridge_owner_pk: Primary key of bridge user to use as owner for created collections
+        :type bridge_owner_pk: int
+        :param content_source_pk: Primary key of content source associated with created activities
+        :param engine_host: Base URL of engine application
+        :param engine_token: API token for engine
+        """
+        if bridge_host:
+            self.bridge_api = BridgeApi(bridge_host, token=bridge_token)
+        if engine_host:
+            self.engine_api = EngineApi(engine_host, token=engine_token)
         self.bridge_owner_pk = bridge_owner_pk
         self.content_source_pk = 1
 
